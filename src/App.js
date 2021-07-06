6d4c424cb51dc9dce5bd096e4aa03977659a41ae
Kurt King <kurtaking@gmail.com>
2021-07-05 20:44:20 -0500

    updating to my name

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is version 2</p>
+        <p>Hello! My Name is Kurt</p>
       </header>
     </div>
   );
